48eb94c524991d32bea79e1aa149b6c023085132
Aaron Sutula <aaron.sutula@gmail.com>
2019-01-26 12:17:30 -0600

    Initial commit adding all the files

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons'
+import icoMoonConfig from '../icomoon_gen/selection.json'
+
+const icon = createIconSetFromIcoMoon(icoMoonConfig)
+
+export default icon
